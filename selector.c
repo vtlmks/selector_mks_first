2dd4de554e32d3bb034b21d1daa47ab1ee708611
Peter Fors <peter.fors@mindkiller.com>
2024-05-26 09:01:07 +0200

    Minor cleanup

--- selector.c
+++ selector.c
@@ -172,13 +172,13 @@ uint32_t mainloop_callback(struct selector_state *state) {
 	state->old_mouse_y = state->shared->mouse_y;
 	state->current_y += mouse_delta;
 
+	// Update selector current_y with keyboard, just in case someone doesn't like the mouse.
 	if(state->shared->keyboard_state[REMAKE_KEY_UP]) {
 		state->current_y -= SPEED_DIVISOR;
 	}
 	if(state->shared->keyboard_state[REMAKE_KEY_DOWN]) {
 		state->current_y += SPEED_DIVISOR;
 	}
-
 
 	// Retrieve max_entry and clamp selector->current_y within bounds
 	uint32_t max_entry = state->remake_count;
@@ -199,7 +199,7 @@ uint32_t mainloop_callback(struct selector_state *state) {
 	render_text(state->remakes, visible_entries, first_entry, state->shared->buffer, state->shared->buffer_width);
 	render_copper_line(state, 78 + 8*9 + 3);
 
-	// Handle mouse button input
+	// Handle Enter key and Mouse Button input
 	if(state->shared->mouse_button_state[REMAKE_MOUSE_BUTTON_LEFT] | state->shared->keyboard_state[REMAKE_KEY_ENTER]) {
 		return (current_entry << 8) | 1; // Use bitwise OR instead of addition
 	}
